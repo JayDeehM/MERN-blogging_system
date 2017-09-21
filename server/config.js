d819880cc04aedd0846877268bff2b5100321ae6
JD <jaad.mateo15@gmail.com>
2017-09-21 14:56:12 +0800

    mongodb fix for heroku deployment

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://jaydeeh:tukmul@mern-shard-00-00-drrlv.mongodb.net:27017,mern-shard-00-01-drrlv.mongodb.net:27017,mern-shard-00-02-drrlv.mongodb.net:27017/test?ssl=true&replicaSet=MERN-shard-0&authSource=admin',
+  mongoURL: process.env.MONGO_URL || 'mongo "mongodb://mern-shard-00-00-drrlv.mongodb.net:27017,mern-shard-00-01-drrlv.mongodb.net:27017,mern-shard-00-02-drrlv.mongodb.net:27017/test?replicaSet=MERN-shard-0" --authenticationDatabase admin --ssl --username jaydeeh --password <tukmul>',
   port: process.env.PORT || 8000,
 };
 
